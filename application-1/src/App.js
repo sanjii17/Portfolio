10f6c22f9cec50ddc3938a5c6f60ef0ec6c4ef29
Saipr14 <saiprasanth67890@gmail.com>
2025-06-15 21:25:45 +0530

    First Commit

---
+++ application-1/src/App.js
@@ -0,0 +1,14 @@
+import Navbar from './Navbar';
+import Content from './Content';
+import './index.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar />
+      <Content />
+    </div>
+  );
+}
+
+export default App;
---
+++ application-1/src/Navbar.js
@@ -0,0 +1,15 @@
+import './index.css';
+
+const Navbar = () => {
+    return (  
+        <nav className="navbar">
+            <h1>SAIPRASANTH A</h1>
+            <div className="links">
+                <a href="/">Home</a>
+                <a href="/create">New Blog</a>
+            </div>
+        </nav>
+    );
+}
+ 
+export default Navbar;
